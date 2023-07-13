730c410f758514b2e34a35d5f85e9b4ac5937ef4
emonbhuiyan <89289628+emonbhuiyan597@users.noreply.github.com>
2023-07-13 14:02:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+function Content() {
+  const [displayText, setDisplayText] = React.useState('');
+
+  const handleButtonClick = () => {
+    const text = document.getElementById('inputText').value;
+    setDisplayText(text);
+  };
+
+  return (
+    <main>
+      <textarea id="inputText" placeholder="Enter some text..."></textarea>
+      <button onClick={handleButtonClick}>Display Text</button>
+      {displayText && <p>{displayText}</p>}
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>About Me - Opportunities don't happen, you create them. </p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Simple React Application" />
+      <Content />
+      <Footer />
     </div>
   );
 }
